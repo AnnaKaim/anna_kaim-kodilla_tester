be6d2baafdab75e10bf48bb11ced034aeaf9d4bd
Anna Kaim <anna.kaim.ak@gmail.com>
2021-03-03 23:09:44 +0100

    First Class changed & Main & MainExtended Classes added

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+       String town = "Grodzisk Mazowiecki";
+       String zipCode = "05-825";
+       double area = 13.19;
+       int population = 30955;
+       char plateFirstLetter = 'W';
+       boolean countyRights = true;
+
+       System.out.println(town);
+       System.out.println(zipCode);
+       System.out.println(area);
+       System.out.println(population);
+       System.out.println(plateFirstLetter);
+       System.out.println(countyRights);
+
+    }
+}
