c2dfe6e95718c5a1265b553f2c15080bd63dd295
Anna Kaim <anna.kaim.ak@gmail.com>
2021-04-04 20:32:09 +0200

    new file

---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class RandomNumbers {
+
+    public int getCountOfRandomNumber() {
+
+        Random random = new Random();
+        int number = 0;
+        int sum = 0;
+        int maxSum = 5000;
+
+        while (sum <= maxSum) {
+            number = random.nextInt(31);
+            number ++;
+        }
+        return number;
+    }
+    public int getMinNumber () {
+        int minNumber = 0;
+
+        return minNumber;
+    }
+
+    public int getMaxNumber () {
+        int maxNumber = 0;
+
+        return maxNumber;
+    }
+
+
+}
