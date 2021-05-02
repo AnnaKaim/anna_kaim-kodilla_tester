9f4d2cfacff568ee60ba938869fffc797e714a31
Anna Kaim <anna.kaim.ak@gmail.com>
2021-05-02 12:58:18 +0200

    ForumStats homework

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,27 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double avg = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> u.getGroup().equals(u.getAge() >=40))
+                .map(u -> u.getNumberOfPost())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println("The average of posts number written by users in the age of 40 or older is: " + avg + ".");
+
+
+        double avg2 = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> u.getGroup().equals(u.getAge() <40))
+                .map(u -> u.getNumberOfPost())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println("The average of posts number written by users younger than 40 years old is: " + avg2 + ".");
+    }
+
+}
