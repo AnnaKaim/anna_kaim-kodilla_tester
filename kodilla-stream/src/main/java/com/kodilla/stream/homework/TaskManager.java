81adb79f118359290b2a12cb036015360f427d72
Anna Kaim <anna.kaim.ak@gmail.com>
2021-05-06 06:17:40 +0200

    fixed TaskManager

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
@@ -6,15 +6,10 @@ import java.util.stream.Collectors;
 
 public class TaskManager {
     public static void main(String[] args) {
-        List<String> deadlines = TaskRepository.getTasksList()
+        List<Task> tasksBeforeDeadline = TaskRepository.getTasksList()
                 .stream()
                 .filter(u -> u.getDeadline().isAfter(LocalDate.now()))
-                .map(TaskManager::getDeadline)
                 .collect(Collectors.toList());
-                System.out.println(deadlines);
-    }
-
-    public static String getDeadline(Task task) {
-        return task.getDeadline();
+                System.out.println(tasksBeforeDeadline);
     }
 }
