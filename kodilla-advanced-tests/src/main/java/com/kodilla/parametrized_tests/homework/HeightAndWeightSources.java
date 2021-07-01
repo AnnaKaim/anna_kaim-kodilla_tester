8800adc8e2c43bfbd9ebd9273bbed5c172f57170
Anna Kaim <anna.kaim.ak@gmail.com>
2021-07-01 17:59:43 +0200

    Calculator BMI homework

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/HeightAndWeightSources.java
@@ -0,0 +1,23 @@
+package com.kodilla.parametrized_tests.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.Arguments;
+
+public class HeightAndWeightSources {
+
+    private List<Person> people = new ArrayList<>();
+
+    static Stream<Arguments> provideHeightAndWeightForTestingBMI() {
+        return Stream.of(
+                Arguments.of(1.67, 59.3),
+                Arguments.of(1.70, 61.1),
+                Arguments.of(1.92, 69.5),
+                Arguments.of(1.82, 97.7),
+                Arguments.of(1.74, 101.3)
+        );
+    }
+
+
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTestSuite {
+
+    private Person individualBMI = new Person();
+
+    @ParameterizedTest
+    @MethodSource(value = "com.kodilla.parametrized_tests.HeightAndWeightSources#provideHeightAndWeightForTestingBMI")
+    public void shouldCalculateBMI(double input, double expected) {
+
+        assertEquals(expected, individualBMI.getBMI(input));
+    }
+
+}
+
+
+
